04a2bede0a1aaf7a019631f5be879f3829b95b53
Zeldodo17 <meza.emilio.2019@cesunbc.edu.mx>
2022-10-31 16:17:21 -0700

    S creó la animación para el caminar del personaje

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -4,25 +4,41 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    public float speedMove = 15;
-    public float jumpSpeed = 20;
+    public float speedMove = 5;
+    public float jumpSpeed = 23;
     public bool enPiso = true;
-    Rigidbody2D rb;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer spr;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKey(KeyCode.D)){
+            if(spr.flipX){
+                spr.flipX = false;
+            }
             transform.position += Vector3.right * speedMove * Time.deltaTime;
+            anim.SetBool("Walking", true);
+        } else {
+            anim.SetBool("Walking", false);
         }
+
         if(Input.GetKey(KeyCode.A)){
+            spr.flipX = true;
             transform.position += Vector3.left * speedMove * Time.deltaTime;
+            anim.SetBool("Walking", true);
+        }else{
+            anim.SetBool("Walking", false);
         }
+
         if(Input.GetKey(KeyCode.W) && enPiso){
             rb.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
             enPiso = false;
