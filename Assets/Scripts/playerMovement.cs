922f59bb1bcb3bea1380bedae4a7a5f3547cb211
Zeldodo17 <meza.emilio.2019@cesunbc.edu.mx>
2022-10-31 18:41:50 -0700

    Se resolvio el bug de la animacion a la derecha

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+    [Header("Speed player")]
     public float speedMove = 5;
     public float jumpSpeed = 23;
+    [Header("Collision")]
     public bool enPiso = true;
     private Rigidbody2D rb;
     private Animator anim;
@@ -21,27 +23,32 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if(Input.GetKey(KeyCode.A)){
+            spr.flipX = true;
+            transform.position += Vector3.left * speedMove * Time.deltaTime;
+            anim.SetBool("Walking_Left", true);
+        }else{
+            anim.SetBool("Walking_Left", false);
+        }
+
         if(Input.GetKey(KeyCode.D)){
             if(spr.flipX){
                 spr.flipX = false;
             }
             transform.position += Vector3.right * speedMove * Time.deltaTime;
-            anim.SetBool("Walking", true);
+            anim.SetBool("Walking_right", true);
         } else {
-            anim.SetBool("Walking", false);
-        }
-
-        if(Input.GetKey(KeyCode.A)){
-            spr.flipX = true;
-            transform.position += Vector3.left * speedMove * Time.deltaTime;
-            anim.SetBool("Walking", true);
-        }else{
-            anim.SetBool("Walking", false);
+            anim.SetBool("Walking_right", false);
         }
 
         if(Input.GetKey(KeyCode.W) && enPiso){
             rb.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
             enPiso = false;
+        }
+
+        if(Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.AltGr)){
+            Debug.Log("CORRER MAS RAPIDO");
         }
     }
 
@@ -50,5 +57,9 @@ public class playerMovement : MonoBehaviour
         if(other.gameObject.tag == "FLOOR"){
             enPiso=true;
         }
+    }
+    void OnCollisionExit(Collision other)
+    {
+     Debug.Log("HA SALTADO");
     }
 }
